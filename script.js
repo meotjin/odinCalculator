00ca87883437b8ddb0203b1389c0eaecc8170969
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-02 20:27:24 +0330

    create operate function

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 function add(number1, number2) {
-    return number1+number2;
+	return number1 + number2;
 }
 
 function subtract(number1, number2) {
-    return number1-number2;
+	return number1 - number2;
 }
 
 function multiply(number1, number2) {
-    return number1*number2;
+	return number1 * number2;
 }
 
-function divide(number1,number2) {
-    return number1/number2;
+function divide(number1, number2) {
+	return number1 / number2;
+}
+
+function operate(operator, number1, number2) {
+	switch (operator) {
+		case "+":
+			return add(number1, number2);
+			break;
+		case "-":
+			return subtract(number1, number2);
+			break;
+		case "x":
+			return multiply(number1, number2);
+			break;
+		case "/":
+			return divide(number1, number2);
+		default:
+			console.log("invalid operator");
+			break;
+	}
 }
