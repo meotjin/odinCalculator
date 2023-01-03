d728873dffcce324cd356ff0c7129083d88cffc1
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-03 09:35:44 +0330

    give operator & equal buttons functionality

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let operation;
 let storedNum1;
-let storedNum2;
-
 
 function add(number1, number2) {
 	return number1 + number2;
@@ -38,34 +36,75 @@ function operate(operator, number1, number2) {
 	}
 }
 
-const screen = document.querySelector('#screen');
+const screen = document.querySelector("#screen");
 
-const buttons = document.querySelectorAll('.number');
-buttons.forEach((b)=>{
-    b.addEventListener('click',(e)=>{
-        screen.textContent += e.target.textContent;
-    })
-})
+const buttons = document.querySelectorAll(".number");
+buttons.forEach((b) => {
+	b.addEventListener("click", (e) => {
+		screen.textContent += e.target.textContent;
+	});
+});
 
-const operators = document.querySelectorAll('.operator');
+const operators = document.querySelectorAll(".operator");
 
-operators.forEach(operator => {
-    operator.addEventListener('click',e => {
-        
-    })
-})
+operators.forEach((operator) => {
+	operator.addEventListener("click", (e) => {});
+});
 
-const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', e => {
-	screen.textContent = '';
+const clearButton = document.querySelector("#clear");
+
+clearButton.addEventListener("click", (e) => {
+	screen.textContent = "";
 	operation = undefined;
 	storedNum1 = undefined;
 	storedNum2 = undefined;
-})
+});
 
-const undoButton = document.querySelector('#undo');
-undoButton.addEventListener('click', e => {
-	let newText = screen.textContent.split('');
+const undoButton = document.querySelector("#undo");
+
+undoButton.addEventListener("click", (e) => {
+	let newText = screen.textContent.split("");
 	newText.pop();
-	screen.textContent = newText.join('');
-})
+	screen.textContent = newText.join("");
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+operatorButtons.forEach((operator) => {
+	if (operator.textContent === '-') {
+		operator.addEventListener("click", () => {
+			if (screen.textContent == "") {
+				screen.textContent += operator.textContent;
+			} else {
+				if (storedNum1 !== undefined && operation !== undefined) {
+					storedNum1 = operate(operation, storedNum1, +screen.textContent);
+				} else {
+					storedNum1 = +screen.textContent;
+				}
+				operation = operator.textContent;
+				screen.textContent = '';
+			}
+		});
+	} else {
+		operator.addEventListener("click", () => {
+			if (storedNum1 !== undefined && operation !== undefined) {
+				storedNum1 = operate(operation, storedNum1, +screen.textContent);
+			} else {
+				storedNum1 = +screen.textContent;
+			}
+			operation = operator.textContent;
+			screen.textContent = '';
+		});
+	}
+});
+
+const equalButton = document.querySelector('.equal');
+
+equalButton.addEventListener('click', e=>{
+	if(storedNum1 !== undefined && operation !== undefined){
+		storedNum1 = operate(operation, storedNum1, +screen.textContent);
+		screen.textContent = storedNum1;
+		storedNum1 = undefined;
+		operation = undefined;
+	}
+});
